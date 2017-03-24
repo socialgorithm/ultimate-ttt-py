438c65cc88c8b817567d4f54fc0cd474c1a6c16e
Bharat Reddy <me@bharatreddy.com>
2017-03-24 00:49:18 +0000

    Updated metadata

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a python game engine that players (and the [Ultimate TicTacToe Server](ultimate-ttt-server)) use to play Ultimate TicTacToe games.
 
-This readme needs to be padded out, but first step is to implement the [API](https://github.com/socialgorithm/ultimate-ttt-js/issues/2)
+Documentation incoming!
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='ultimate_ttt',
       version='0.1',
@@ -8,5 +8,7 @@ setup(name='ultimate_ttt',
       author='Bharat Reddy',
       author_email='me@bharatreddy.com',
       url='https://github.com/socialgorithm/ultimate-ttt-py',
-      packages=['ultimate_ttt']
+      packages=['ultimate_ttt'],
+	  setup_requires=['pytest-runner'],
+	  tests_require=['pytest']
      )
