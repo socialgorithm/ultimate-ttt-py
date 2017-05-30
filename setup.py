6312de7757d4f56f0bec03dce0fe968ad90d9ce4
Bharat Reddy <me@bharatreddy.com>
2017-05-30 17:58:48 +0100

    Moved player to player repository

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@
 from setuptools import setup
 
 setup(name='ultimate_ttt',
-      version='0.1',
+      version='1.0',
       description='Game engine for Ultimate TicTacToe games',
-      author='Bharat Reddy',
-      author_email='me@bharatreddy.com',
+      author='socialgorithm',
       url='https://github.com/socialgorithm/ultimate-ttt-py',
-      packages=['ultimate_ttt', 'ultimate_ttt_player'],
+      packages=['ultimate_ttt'],
 	  setup_requires=['pytest-runner'],
 	  tests_require=['pytest']
      )
--- ultimate_ttt_player/__init__.py
+++
@@ -1,2 +0,0 @@
-from .ultimate_ttt_player import UltimateTTTPlayer
-from .random_player import Random
