64bdc3b238f9bc75a7bffb00a4c9ee7fd864ab39
Bharat Reddy <me@bharatreddy.com>
2018-05-23 17:40:04 +0100

    Bump to 1.3, because why not

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(name='ultimate_ttt',
-      version='1.2',
+      version='1.3',
       description='Game engine and sample player for Ultimate TicTacToe games',
       long_description=readme,
       author='socialgorithm',
@@ -16,6 +16,6 @@ setup(name='ultimate_ttt',
       packages=['engine','players'],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
-      download_url="https://github.com/socialgorithm/ultimate-ttt-py/archive/1.2.tar.gz",
+      download_url="https://github.com/socialgorithm/ultimate-ttt-py/archive/1.3.tar.gz",
       keywords='ultimate tictactoe uttt ultimate-ttt'
 )
