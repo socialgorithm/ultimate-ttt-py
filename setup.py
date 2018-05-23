eb2bebae8acebc7ed4e622f39ce744f2bdf4dd1c
Bharat Reddy <me@bharatreddy.com>
2018-05-23 17:35:30 +0100

    Update long description

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 
 from setuptools import setup
 
+
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(name='ultimate_ttt',
       version='1.2',
       description='Game engine and sample player for Ultimate TicTacToe games',
+      long_description=readme,
       author='socialgorithm',
       author_email='hello@socialgorithm.org',
       url='https://github.com/socialgorithm/ultimate-ttt-py',
@@ -12,5 +17,5 @@ setup(name='ultimate_ttt',
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       download_url="https://github.com/socialgorithm/ultimate-ttt-py/archive/1.2.tar.gz",
-      keywords = ['ultimate','tictactoe','uttt','ultimate-ttt']
-      )
+      keywords='ultimate tictactoe uttt ultimate-ttt'
+)
