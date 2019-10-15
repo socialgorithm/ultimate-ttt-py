106c270a4bae9d61e367740212bc21ac03755931
unknown <joffat@users.noreply.github.com>
2019-10-15 22:41:22 +0100

    Better Align All Players

--- players/random.py
+++ players/random.py
@@ -5,14 +5,89 @@ from players.stdout import StdOutPlayer
 
 
 class Random(StdOutPlayer):
+    #
+    # New Game Has Been Started.
+    # Do Whatever You Need To Do For The Player To Be Ready To Play
+    #
     def __init__(self):
         super().__init__()
 
-    def get_my_move(self):  # -> Tuple[MainBoardCoords, SubBoardCoords]
-        main_board_coords = self.pick_next_main_board_coords()
-        sub_board = self.main_board.get_sub_board(main_board_coords)
+    #
+    # You are the First To Move
+    # Place Whereever You Want
+    # You Get To Pick The Board
+    #
+    # Returns Tuple[MainBoardCoords, SubBoardCoords] Position coordinates {board: [row, col], cell: [row, col]}
+    #
+    def onMove(self):
+        board = self.pick_next_main_board_coords()
+        sub_board = self.main_board.get_sub_board(board)
+        move = self.pick_random_sub_board_coords(sub_board)
+        return board, move
+
+    #
+    # Opponent Has Moved.
+    # You Have Opponents Board
+    # You Have Opponents Move
+    #
+    # You Must Respond With Board Matching Opponents Move
+    # Unless That Board Is Complete
+    #
+    # Returns Tuple[MainBoardCoords, SubBoardCoords] Position coordinates {board: [row, col], cell: [row, col]}
+    #
+    def onOpponent(self, board, move):
+        sub_board = self.main_board.get_sub_board(move)
+        if sub_board.is_finished == True:
+            return self.onMove()
         sub_board_coords = self.pick_random_sub_board_coords(sub_board)
-        return main_board_coords, sub_board_coords
+        return move, sub_board_coords
+
+    #
+    # Game Is Over.
+    # You May Wish To Change Stratagy For The New Game.
+    # Your Opponent Has Not Changed.
+    # 
+    # result Either 'win' | 'lose' | 'tie'
+    # board Last Opponent Board identifier [row, col]
+    # move Last Opponent Cell identifier [row, col]
+    #
+    def gameOver(self, result, board, move):
+        if result == "win":
+            # DO SOMETHING FOR WIN
+            pass
+        elif result == "lose":
+            # DO SOMETHING FOR LOST
+            pass
+        elif result == "tie":
+            # DO SOMETHING FOR TIE
+            pass
+        return
+
+    #
+    # Match Is Over.
+    # You Will Soon Have A New Opponent.
+    # 
+    # result Either 'win' | 'lose' | 'tie'
+    #
+    def matchOver(self, result):
+        if result == "win":
+            # DO SOMETHING FOR WIN
+            pass
+        elif result == "lose":
+            # DO SOMETHING FOR LOST
+            pass
+        elif result == "tie":
+            # DO SOMETHING FOR TIE
+            pass
+        return
+
+    #
+    # Game Lost.
+    # You Have Timed Out.
+    #
+    def timeout(self):
+        # DO SOMETHING FOR TIMEOUT
+        return
 
     def pick_next_main_board_coords(self) -> MainBoardCoords:
         if self.main_board.sub_board_next_player_must_play is None:
