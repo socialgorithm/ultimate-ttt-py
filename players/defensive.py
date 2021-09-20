558a1149665c7edd35bd7f1f9f72a5294da24cb6
gyorokpeter <gyorokpeter@freemail.hu>
2021-09-20 08:46:18 +0100

    fix naming convention

--- players/defensive.py
+++ players/defensive.py
@@ -13,7 +13,7 @@ class Defensive(StdOutPlayer):
     def get_my_move(self):  # -> Tuple[MainBoardCoords, SubBoardCoords]
         self.move += 1
         validBoards = self.main_board.get_playable_coords()
-        weightedMoves = [x for x in [self.getMoveForBoard(x) for x in validBoards] if x is not None]
+        weightedMoves = [x for x in [self.get_move_for_board(x) for x in validBoards] if x is not None]
         if 0<len(weightedMoves):
             return weightedMoves[0]
         #fall back to the first available logic
@@ -22,12 +22,12 @@ class Defensive(StdOutPlayer):
         sub_board_coords = sub_board.get_playable_coords()[0]
         return main_board_coords, sub_board_coords
 
-    def getMoveForBoard(self, board):
+    def get_move_for_board(self, board):
         subBoard = self.main_board.get_sub_board(board)
-        opponentWinningPositions = self.getCloseablePositions(board, Player.OPPONENT)
+        opponentWinningPositions = self.get_closeable_positions(board, Player.OPPONENT)
         if 0<len(opponentWinningPositions):
             return (board, opponentWinningPositions[0])
-        myWinningPositions = self.getCloseablePositions(board, Player.ME)
+        myWinningPositions = self.get_closeable_positions(board, Player.ME)
         if 0<len(myWinningPositions):
             return (board, myWinningPositions[0])
         return None
@@ -39,14 +39,14 @@ class Defensive(StdOutPlayer):
     def _columns(self, board):
         return [self._column(board,x) for x in range(len(board[0]))]
 
-    def _majorDiagonal(self, board):
+    def _major_diagonal(self, board):
         return [board[x][x] for x in range(len(board[0]))]
 
-    def _minorDiagonal(self, board):
+    def _minor_diagonal(self, board):
         n = len(board[0])
         return [board[x][n-1-x] for x in range(n)]
 
-    def getWinningPosition(self, cells, playerNo):
+    def get_winning_position(self, cells, playerNo):
         playerScore = sum([1 if x.played_by == playerNo else 0 for x in cells])
         if playerScore == len(cells)-1:
             notPlayed = [i for i,x in enumerate(cells) if x.played_by == Player.NONE]
@@ -54,12 +54,12 @@ class Defensive(StdOutPlayer):
                 return notPlayed[0]
         return None
 
-    def getCloseablePositions(self, board, playerNo):
+    def get_closeable_positions(self, board, playerNo):
         subBoard = self.main_board.get_sub_board(board)
-        rows=([SubBoardCoords(i,x) for i,x in enumerate([self.getWinningPosition(row, playerNo) for row in subBoard]) if x is not None]
-            + [SubBoardCoords(x,i) for i,x in enumerate([self.getWinningPosition(column, playerNo) for column in self._columns(subBoard)]) if x is not None]
-            + (lambda x:[SubBoardCoords(x,x)] if x is not None else [])(self.getWinningPosition(self._majorDiagonal(subBoard), playerNo))
-            + (lambda x:[SubBoardCoords(x,len(subBoard[0])-1-x)] if x is not None else [])(self.getWinningPosition(self._minorDiagonal(subBoard), playerNo)))
+        rows=([SubBoardCoords(i,x) for i,x in enumerate([self.get_winning_position(row, playerNo) for row in subBoard]) if x is not None]
+            + [SubBoardCoords(x,i) for i,x in enumerate([self.get_winning_position(column, playerNo) for column in self._columns(subBoard)]) if x is not None]
+            + (lambda x:[SubBoardCoords(x,x)] if x is not None else [])(self.get_winning_position(self._major_diagonal(subBoard), playerNo))
+            + (lambda x:[SubBoardCoords(x,len(subBoard[0])-1-x)] if x is not None else [])(self.get_winning_position(self._minor_diagonal(subBoard), playerNo)))
         return [x for x in rows if x is not None]
 
     def timeout(self):
